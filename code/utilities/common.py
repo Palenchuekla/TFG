26c7379d07d4e152ca9969c8ae2e50b417f329d2
Palenchuekla <plnzlaj@gmail.com>
2024-06-19 17:25:51 +0200

    Realloacting reproducibility function.

--- code/utilities/common.py
+++ code/utilities/common.py
@@ -3,6 +3,23 @@ import pandas as pd
 import numpy as np
 from IPython import display
 from sklearn.metrics import confusion_matrix
+import random
+import torch
+
+
+# Reproducibility
+def reproducibility(seed=1234):
+  '''
+    Fuction to iniatialize every random process related to PyTorch: parameters initialization and batching order.
+    Also applies to Numpy.
+  '''
+  torch.manual_seed(seed)
+  np.random.seed(seed)
+  random.seed(seed)
+  torch.cuda.manual_seed_all(seed)
+  torch.backends.cudnn.deterministic = True
+  torch.backends.cudnn.benchmark = False
+  os.environ['PYTHONHASHSEED'] = str(seed)
 
 def sensitivity_score(y_true, y_pred):
     '''
